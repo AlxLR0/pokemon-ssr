2cfdb4df5cbf9a54cc77e25f3169f279a322e3b7
Alejandro <alex.lopez.rubio2600@gmail.com>
2025-08-14 15:10:22 -0600

    creacion de la app, componenter y estilos del navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about-page/about-page.component').then(m => m.AboutPageComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact-page/contact-page.component').then(m => m.contactPageComponent)
+    },
+    {
+        path: 'pricing',
+        loadComponent: () => import('./pages/pricing-page/pricing-page.component').then(m => m.pricingPageComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'about'
+    }
+
+];
