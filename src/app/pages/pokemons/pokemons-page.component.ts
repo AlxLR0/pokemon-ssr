7f6761e1818032a8500f7650a98d3b5464a3741f
Alejandro <alex.lopez.rubio2600@gmail.com>
2025-08-21 15:13:56 -0600

    feat: agregar paginación y mejorar la navegación en la página de Pokémons

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
-import { Title, Meta } from '@angular/platform-browser';
-import { title } from 'process';
+import { Title, Meta } from '@angular/platform-browser'; 
+import {toSignal} from '@angular/core/rxjs-interop';
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-pokemons-page',
@@ -19,6 +21,17 @@ export class PokemonsPageComponent implements OnInit {
   private pokemonsService = inject(PokemonsService);
   public pokemons = signal<SimplePokemon[]>([]);
 
+  private route = inject(ActivatedRoute); 
+  private router = inject(Router);
+
+  public currentPage = toSignal<number>(
+    this.route.queryParamMap.pipe(
+      map((params) => params.get('page') ?? '1'),
+      map((page)=>(isNaN(+page)?1 :+page)), // Convierte el valor a número, si no es un número, devuelve 1
+      map((page)=>Math.max(1,page)) // Asegura que el valor sea al menos 1
+    )
+  )
+
   ngOnInit(): void {
     this.title.setTitle('Pokemons');
     this.meta.addTag({ name: 'description', content: 'This is the pokemon page' });
@@ -31,14 +44,29 @@ export class PokemonsPageComponent implements OnInit {
   }
 
   //este metodo se encarga de cargar los pokemones
-  public loadPokemons(Page = 0){
-    this.pokemonsService.loadPage(Page)//esto es una llamada al servicio que se encarga de cargar los pokemones
-    .subscribe(pokemons =>{
+public loadPokemons(page = 0) {
+  // Calcula la página a cargar en base a la actual + el offset recibido (page)
+  const pageToLoad = this.currentPage()! + page;
+
+  // Evita que la página sea menor a 1
+  if (pageToLoad < 1) return;
+
+  this.isLoading.set(true); // Inicia la carga
+
+  this.pokemonsService.loadPage(pageToLoad) //esto es una llamada al servicio que se encarga de cargar los pokemones
+    .pipe(
+      tap(() =>
+        // 👇 para la nevegacion
+         this.router.navigate([], { queryParams: { page: pageToLoad } })
+      ),
+      tap(()=> this.title.setTitle(`Pokedex Page ${pageToLoad}`)), // Cambia el título de la página
+    )
+    .subscribe(pokemons => {
       // console.log('on init');
-      this.pokemons.set(pokemons);//se setea el valor de los pokemones
-      
-    })
-  }
+      this.pokemons.set(pokemons); //se setea el valor de los pokemones
+      this.isLoading.set(false); // Termina la carga
+    });
+}
 
 
 
