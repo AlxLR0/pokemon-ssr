f521aa16d3652487c9067e20ab2a49a888a3e84d
Alejandro <alex.lopez.rubio2600@gmail.com>
2025-08-26 14:42:49 -0600

    feat: mejorar la página de detalles de Pokémon con audio y optimización de SEO

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -3,6 +3,8 @@ import { Pokemon } from '../../pokemons/interfaces/pokemon.interface';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemon-page',
@@ -14,6 +16,8 @@ import { CommonModule } from '@angular/common';
 export class PokemonPageComponent implements OnInit {
   private pokemonsService = inject(PokemonsService);
   private route = inject(ActivatedRoute);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   // señal reactiva que guarda el pokemon
   public pokemon = signal<Pokemon | null>(null); 
@@ -23,6 +27,22 @@ export class PokemonPageComponent implements OnInit {
     if (!id) return;
 
     // obtener el pokemon desde el servicio
-    this.pokemonsService.loadPokemon(id).subscribe(p => this.pokemon.set(p));
+    this.pokemonsService.loadPokemon(id)
+    .pipe(
+      tap( ({name, id}) => {
+        const pageTitle = `#${id} - ${name}`;// esto es para el título de la página y el nombre del pokemon
+        const pageDescription = `Información sobre ${name}`; // esto es para la descripción de la página
+        // SEO: actualizar el título y las meta etiquetas
+        //esto sirve para que cada página de pokemon tenga su propio título y descripción en los motores de búsqueda
+        this.title.setTitle(pageTitle); // Título de la página
+        this.meta.updateTag({ name: 'description', content: pageDescription });// Descripción
+        this.meta.updateTag({ name: 'og:title', content: pageTitle}); // Open Graph título
+        this.meta.updateTag({ name: 'og:description', content: pageDescription}); // Open Graph descripción
+        this.meta.updateTag({ name: 'og:image', content: 
+          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`, // Open Graph imagen
+        });// Open Graph imagen
+      })
+    )
+    .subscribe(p => this.pokemon.set(p));
   } 
 }
